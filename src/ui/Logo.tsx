7e460dfef478433d8cfa1812e442aadca3627783
bhargav <bhargav.j@crestinfosystems.net>
2025-07-04 13:45:04 +0530

    feat: Update logo images in Logo component for improved branding
    - Replaced logo image paths in the Logo component to use high-resolution versions, enhancing visual quality and brand representation.

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,7 +1,7 @@
 function Logo({expanded = false}: { expanded?: boolean }) {
   return (
     <div className="text-center">
-      <img src={expanded ? "/logo.png" : "/safetech.png"} alt="Logo" className={!expanded ? "h-[60px] mx-auto" : "w-[115px] mx-auto"} />
+      <img src={expanded ? "/Safetech Logo High Resolution No Background.png" : "/Safetech Logo (Salamander Only) (1).png"} alt="Logo" className={!expanded ? "h-[60px] mx-auto" : "w-[115px] mx-auto"} />
     </div>
   );
 }
